cd5817136dc44dcb71053b6bfbed6b8ed7a63f8a
j0303p <j0303p@gmail.com>
2024-07-30 15:22:38 +0900

    da

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+import { inter } from './ui/fonts';
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
